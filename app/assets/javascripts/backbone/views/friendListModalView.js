74c5e4d3d771f9af6c4723550a8027bc0b5581aa
alevsxm <alexmlevine@gmail.com>
2014-09-11 10:13:15 -0400

    minor css tweaks and new friend list modal view

---
+++ app/assets/javascripts/backbone/views/friendListModalView.js
@@ -0,0 +1,19 @@
+App.FriendListView = Backbone.View.extend({
+  el: '#friends',
+  initialize: function() {
+    console.log('New Friend List View');
+    this.listenTo(this.collection, 'reset', this.addAll);
+  },
+  addOne: function(friend) {
+    var friendView = new App.FriendView({model: friend});
+    this.$el.append(friendView.$el);
+    console.log(this.$el);
+    console.log(friendView.$el);
+  },
+  addAll: function() {
+    this.collection.each(function(friend) {
+      this.addOne(friend);
+    }, this);
+  }
+
+});
