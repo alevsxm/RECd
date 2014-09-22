5fb5a3e630c45b87f2b03fa45d8a8aef66d6f66f
alevsxm <alexmlevine@gmail.com>
2014-09-22 11:28:42 -0400

    tried to fix confirm, added background image to landing page

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,6 +5,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       @user.confirm!
+      @user.skip_reconfirmation! #skips comfirmation email when users sign in through facebook
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
